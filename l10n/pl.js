bd1eb7a32a215d36026cf96bc5cddc741723c51b
Xavier Beurois <xbeurois@djazz-univ.net>
2015-06-10 21:25:57 +0200

    Update PL and RU translation files

--- l10n/pl.js
+++ l10n/pl.js
@@ -14,12 +14,16 @@ OC.L10N.register
     "ocdownloader",
     {
 		"Unable to find the GID for this download ...": "Nie znaleziono GID dla tego pobrania ...",
-		"No Torrent Files": "Brak plików torrent",
-		"Upload": "Prześlij",
-		"Waiting": "Oczekiwanie",
-		"Invalid URL. Please check the address of the file ...": "Nieprawidłowy URL. Sprawdź adres pliku ...",
-		"Launch YouTube Download": "Rozpocznij pobieranie z Youtube",
-		"Select a file.torrent": "Wybierz plik .torrent",
-		"Please select a file.torrent": "Wybierz plik .torrent"
+	    "No Torrent Files": "Brak plików torrent",
+	    "Upload": "Prześlij",
+	    "Waiting": "Oczekiwanie",
+	    "Invalid URL. Please check the address of the file ...": "Nieprawidłowy URL. Sprawdź adres pliku ...",
+	    "Launch YouTube Download": "Rozpocznij pobieranie z Youtube",
+	    "Select a file.torrent": "Wybierz plik .torrent",
+	    "Please select a file.torrent": "Wybierz plik .torrent",
+	    "No downloads in the queue ...": "Brak pobierań w kolejce...",
+	    "Removed": "Usunięte",
+	    "Active": "Aktywne",
+	    "Paused": "Wstrzymane"
 	}
 )
