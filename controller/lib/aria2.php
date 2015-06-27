45771befde3de4f9d9b1c60aed44e8270c1be0f0
Xavier Beurois <xbeurois@djazz-univ.net>
2015-06-27 04:26:38 +0200

    Fix a bug in the lib aria2 using the AddTorrent function

--- controller/lib/aria2.php
+++ controller/lib/aria2.php
@@ -18,7 +18,7 @@ class Aria2
     public static function __callStatic ($Name, $Args)
     {
         self::$Server = 'http://127.0.0.1:6800/jsonrpc';
-        $Args = self::RebuildArgs ($Args);
+        $Args = (strcmp ($Name, 'AddTorrent') == 0 ? self::RebuildTorrentArgs ($Args) : self::RebuildArgs ($Args));
         
         self::Load ();
         
@@ -26,7 +26,7 @@ class Aria2
             'jsonrpc'   => '2.0',
             'id'        => 'ocdownloader',
             'method'    => 'aria2.' . lcfirst ($Name),
-            'params'    =>  strcmp ($Name, 'AddTorrent') == 0 ? Array ($Args[0], Array (), $Args [1]) : $Args
+            'params'    => $Args
         );
         
         return json_decode (self::Request ($Data), 1);
@@ -65,6 +65,23 @@ class Aria2
         if (isset ($Args[1]['Params']))
         {
             $RebuildArgs[1] = $Args[1]['Params'];
+        }
+        
+        return $RebuildArgs;
+    }
+    
+    private static function RebuildTorrentArgs ($Args)
+    {
+        if (isset ($Args[2]['Server']) && !is_null ($Args[2]['Server']))
+        {
+            self::$Server = $Args[2]['Server'];
+        }
+        
+        $RebuildArgs = Array ($Args[0], Array ());
+        
+        if (isset ($Args[2]['Params']))
+        {
+            $RebuildArgs[2] = $Args[2]['Params'];
         }
         
         return $RebuildArgs;
