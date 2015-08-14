7b875dd3301734041d8ec263f2eb1e56f30340b3
Xavier Beurois <xbeurois@djazz-univ.net>
2015-08-14 23:58:45 +0200

    Change the application licence to Creative Commons BY-SA

--- js/ocdownloader.min.js
+++ js/ocdownloader.min.js
@@ -1,7 +1,7 @@
 /**
  * ownCloud - OCDLR
  *
- * This file is licensed under the Affero General Public License version 3 or
+ * This file is licensed under the Creative Commons BY-SA License version 3 or
  * later. See the COPYING file.
  *
  * @author Xavier Beurois <www.sgc-univ.net>
