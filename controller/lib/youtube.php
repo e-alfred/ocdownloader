f524814168e311968f725d2afc93410c156a05a9
Loki3000 <github@labcms.ru>
2016-04-07 12:37:48 +0400

    Support for youtube playlists url
    Support for youtube playlists url such a:
    https://www.youtube.com/watch?v=n-NEcGEApTg&list=PLM6kePQ4tAcgQZJF8D-YE0AZJpGmLQtPh&index=2
    or
    https://www.youtube.com/watch?v=n-NEcGEApTg&list=PLM6kePQ4tAcgQZJF8D-YE0AZJpGmLQtPh

--- controller/lib/youtube.php
+++ controller/lib/youtube.php
@@ -49,27 +49,43 @@ class YouTube
 		
 		$Output = shell_exec ($this->YTDLBinary . ' -i \'' . $this->URL . '\' --get-url --get-filename' . ($ExtractAudio ? ' -f bestaudio -x' : ' -f best') . ($this->ForceIPv4 ? ' -4' : '') . (is_null ($Proxy) ? '' : $Proxy));
 		
+		$index=(preg_match('/&index=(\d+)/', $this->URL, $current))?$current[1]:1;
+		
 		if (!is_null ($Output))
 		{
 			$Output = explode ("\n", $Output);
+
 			if (count ($Output) >= 2)
 			{
 				$OutProcessed = Array ();
+				$current_index=1;
 				for ($I = 0; $I < count ($Output); $I++)
 				{
-					if (strlen (trim ($Output[$I])) > 0)
+					if (mb_strlen (trim ($Output[$I])) > 0)
 					{
-						if (strpos (urldecode ($Output[$I]), 'https://') == 0 && strpos (urldecode ($Output[$I]), '&mime=video/') !== false)
+						if (mb_strpos (urldecode ($Output[$I]), 'https://') == 0 && mb_strpos (urldecode ($Output[$I]), '&mime=video/') !== false)
 						{
 							$OutProcessed['VIDEO'] = $Output[$I];
 						}
-						elseif (strpos (urldecode ($Output[$I]), 'https://') == 0 && strpos (urldecode ($Output[$I]), '&mime=audio/') !== false)
+						elseif (mb_strpos (urldecode ($Output[$I]), 'https://') == 0 && mb_strpos (urldecode ($Output[$I]), '&mime=audio/') !== false)
 						{
 							$OutProcessed['AUDIO'] = $Output[$I];
 						}
 						else
 						{
 							$OutProcessed['FULLNAME'] = $Output[$I];
+						}
+					}
+					if ((!empty($OutProcessed['VIDEO']) || !empty($OutProcessed['AUDIO'])) && !empty($OutProcessed['FULLNAME']))
+					{
+						if ($index==$current_index)
+						{
+							break;
+						}
+						else
+						{
+							$OutProcessed = Array ();
+							$current_index++;
 						}
 					}
 				}
