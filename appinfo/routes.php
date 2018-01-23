4ccd063bef5106b9d7a3e3b55958f1cbe92820f6
Alfred Egger <alfred.egger@gmx.at>
2018-01-23 17:57:28 +0100

    Fix deprecated call for external API

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -22,21 +22,21 @@ $Application->registerRoutes($this, array(
         ['name' => 'Index#Waitings', 'url' => '/waitings', 'verb' => 'GET'],
         ['name' => 'Index#Stopped', 'url' => '/stopped', 'verb' => 'GET'],
         ['name' => 'Index#Removed', 'url' => '/removed', 'verb' => 'GET'],
-        
+
         // HttpDownloader
         ['name' => 'HttpDownloader#Add', 'url' => '/httpdownloader/add', 'verb' => 'POST'],
-        
+
         // FtpDownloader
         ['name' => 'FtpDownloader#Add', 'url' => '/ftpdownloader/add', 'verb' => 'POST'],
-        
+
         // YTDownloader
         ['name' => 'YTDownloader#Add', 'url' => '/ytdownloader/add', 'verb' => 'POST'],
-        
+
         // BTDownloader
         ['name' => 'BTDownloader#Add', 'url' => '/btdownloader/add', 'verb' => 'POST'],
         ['name' => 'BTDownloader#ListTorrentFiles', 'url' => '/btdownloader/listtorrentfiles', 'verb' => 'POST'],
         ['name' => 'BTDownloader#UploadFiles', 'url' => '/btdownloader/uploadfiles', 'verb' => 'POST'],
-        
+
         // Queue
         ['name' => 'Queue#Get', 'url' => '/queue/get', 'verb' => 'POST'],
         ['name' => 'Queue#Count', 'url' => '/queue/count', 'verb' => 'POST'],
@@ -48,15 +48,15 @@ $Application->registerRoutes($this, array(
         ['name' => 'Queue#UnPause', 'url' => '/queue/unpause', 'verb' => 'POST'],
         ['name' => 'Queue#RemoveAll', 'url' => '/queue/removeall', 'verb' => 'POST'],
         ['name' => 'Queue#CompletelyRemoveAll', 'url' => '/queue/completelyremoveall', 'verb' => 'POST'],
-        
+
         // AdminSettings
         ['name' => 'AdminSettings#Save', 'url' => '/adminsettings/save', 'verb' => 'POST'],
         ['name' => 'AdminSettings#Get', 'url' => '/adminsettings/get', 'verb' => 'POST'],
-        
+
         // PersonalSettings
         ['name' => 'PersonalSettings#Save', 'url' => '/personalsettings/save', 'verb' => 'POST'],
         ['name' => 'PersonalSettings#Get', 'url' => '/personalsettings/get', 'verb' => 'GET'],
-        
+
         // Updater
         ['name' => 'Updater#Check', 'url' => '/updater/check', 'verb' => 'GET']
     ]
@@ -67,7 +67,7 @@ $APIBasePath = '/apps/ocdownloader/api/';
     'POST',
     $APIBasePath.'version',
     function ($URLParams) {
-        return new \OC_OCS_Result(\OCA\ocDownloader\Controller\Lib\API::checkAddonVersion($_POST['AddonVersion']));
+        return new \OC\OCS\Result(\OCA\ocDownloader\Controller\Lib\API::checkAddonVersion($_POST['AddonVersion']));
     },
     'ocdownloader',
     \OC_API::USER_AUTH
@@ -77,7 +77,7 @@ $APIBasePath = '/apps/ocdownloader/api/';
     'GET',
     $APIBasePath.'queue/get',
     function ($URLParams) {
-        return new \OC_OCS_Result(\OCA\ocDownloader\Controller\Lib\API::getQueue());
+        return new \OC\OCS\Result(\OCA\ocDownloader\Controller\Lib\API::getQueue());
     },
     'ocdownloader',
     \OC_API::USER_AUTH
@@ -87,7 +87,7 @@ $APIBasePath = '/apps/ocdownloader/api/';
     'POST',
     $APIBasePath.'add',
     function ($URLParams) {
-        return new \OC_OCS_Result(\OCA\ocDownloader\Controller\Lib\API::add($_POST['URL']));
+        return new \OC\OCS\Result(\OCA\ocDownloader\Controller\Lib\API::add($_POST['URL']));
     },
     'ocdownloader',
     \OC_API::USER_AUTH
