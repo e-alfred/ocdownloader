add03183af370445d5e3d0ed76fe8b71f6eeb25f
wartw98 <wartw98@gmail.com>
2018-05-23 06:07:16 +0800

    Add files via upload

---
+++ l10n/zh_TW.js
@@ -0,0 +1,36 @@
+/**
+ * ownCloud - ocDownloader
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the LICENSE file.
+ *
+ * @author Xavier Beurois <www.sgc-univ.net>
+ * @Translation authors : Young You, dzxx36gyy (顾益阳), whatot huang
+ * @copyright Xavier Beurois 2015
+ */
+
+OC.L10N.register
+(
+    "ocdownloader",
+	{
+        "Unable to find the GID for this download ...": "無法找到現在的GID …",
+        "No Torrent Files": "沒有BT文件",
+        "Upload": "上傳",
+        "Waiting": "請稍後",
+        "Invalid URL. Please check the address of the file ...": "URL無效，請檢查網址是否正確…",
+        "Launch YouTube Download": "執行 YouTube 下載",
+        "Select a file.torrent": "選擇一個BT文件(*.torrent) ",
+        "Please select a file.torrent": "請選擇一個BT文件(*.torrent) ",
+        "No downloads in the queue ...": "目前沒有任何下載任務 …",
+        "Removed": "已移除",
+        "Active": "啟動",
+        "Paused": "暫停",
+        "HTTP": "HTTP",
+        "HTTPS": "HTTPS",
+        "FTP": "FTP",
+        "FTPS": "FTPS",
+        "BitTorrent": "BitTorrent",
+        "YT Audio": "YT 音頻",
+        "YT Video": "YT 影片"
+    }
+)
