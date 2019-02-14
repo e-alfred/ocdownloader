628bb55eaaf483755d17eabbc345fb818af02f2d
Pablo Castorino <castorinop@gmail.com>
2019-02-14 12:15:49 -0300

    Become DB Service

---
+++ lib/Service/DBService.php
@@ -0,0 +1,55 @@
+<?php 
+
+namespace OCA\ocDownloader\Service;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use OCP\DB\QueryBuilder\IQueryBuilder;
+use OCP\IDBConnection;
+use OCP\Security\ICrypto;
+
+use OCP\IUser;
+
+/**
+ * Stores the mount config in the database
+ */
+class DBService {
+	
+	/**
+	 * @var IDBConnection
+	 */
+	private $connection;
+
+  /**
+	 * DBService constructor.
+	 *
+	 * @param ICrypto $crypto
+	 */
+	public function __construct(IDBConnection $connection) {
+		$this->connection = $connection;
+  }
+  
+  /**
+	 * @param IUser $user
+	 * @return array
+	 */
+	public function getQueueByUser(IUser $user) {
+    
+    $builder = $this->connection->getQueryBuilder();
+    
+		$query = $builder->select('*')
+			->from('ocdownloader_queue')
+			->where(
+        $builder->expr()->eq('UID', $builder->createNamedParameter($user->getUID(), IQueryBuilder::PARAM_STR))
+      )
+			->andwhere(
+				$builder->expr()->in('STATUS', $builder->createNamedParameter([0,1,2,3,4], IQueryBuilder::PARAM_INT_ARRAY))
+      )
+			->andwhere(
+				$builder->expr()->in('IS_CLEANED', $builder->createNamedParameter([0,1], IQueryBuilder::PARAM_INT_ARRAY))
+      );
+		
+    $result = $query->execute();
+    
+    return $result;
+    
+	}
+}
